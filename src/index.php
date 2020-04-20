b800bb76731d7003fcdadbdd32173185ef7199fc
Michael Hernandez <gammabeta010@gmail.com>
2020-04-20 22:17:21 +0200

    Agguinto Spotify logo nel header

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>"Full Stack" - Dischi</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.3/handlebars.min.js" charset="utf-8"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="../dist/css/app.css">
+    </head>
+    <body>
+
+        <header>
+            <div class="container">
+                <img src="img/logo.png" alt="">
+            </div>
+
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
